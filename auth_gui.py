f04f88ff8f9468f54eef058d7c2a4e469541dc0d
Gabriel Dugny <gabriel.dugny@gmail.com>
2017-03-16 10:22:34 +0100

    Test de style sur l'écran de connexions
    Logo Twitter Copyright Twitter et tout

--- auth_gui.py
+++ auth_gui.py
@@ -4,24 +4,39 @@ from tkinter.ttk import *
 root = Tk()
 root.title("Connexion")
 
-cadre = Frame(root)
-cadre.grid(row=0, column=0)
+style = Style()
+style.configure("BW.TLabel", foreground="white", background="#343232")
+style.configure("A.BW.TLabel", foreground="white", background="#565656")
+
+fenetre = Frame(root, style="BW.TLabel")
+fenetre.pack()
+
+cadre = Frame(fenetre, style="BW.TLabel")
+cadre.grid(row=0, column=0, padx=50, pady=50)
 
 user = StringVar()
 password = StringVar()
 
-logophoto = PhotoImage(file="Twitter_Logo_Blue.png")
-logo = Label(cadre, image=logophoto)
+logophoto = PhotoImage(file="Twitter_Logo_Blue_Cropped.png")
+logo = Label(cadre, width=11, image=logophoto, style="BW.TLabel")
 logo.image = logophoto
-userlabel = Label(cadre, text="Nom d'utilisateur : ")
-userentry = Entry(cadre, textvariable="user")
-passlabel = Label(cadre, text="Mot de passe : ")
-passentry = Entry(cadre, textvariable="password", show="*")
 
-logo.grid(row=0, column=0, columnspan=1)
-userlabel.grid(row=1, column=0,sticky="ew", padx=15)
-userentry.grid(row=1, column=1,sticky="ew", padx=15)
-passlabel.grid(row=2, column=0, sticky="ew", padx=15)
-passentry.grid(row=2, column=1, sticky="ew", padx=15, pady=15)
+cadretexte = Frame(cadre, style="A.BW.TLabel")
+cadretexte.grid(row=1, column=0, columnspan=2)
+
+titrelabel = Label(cadretexte, text="Bienvenue", style="A.BW.TLabel")
+userlabel = Label(cadretexte, text="Nom d'utilisateur : ", style="A.BW.TLabel")
+userentry = Entry(cadretexte, textvariable="user", style="A.BW.TLabel")
+passlabel = Label(cadretexte, text="Mot de passe : ", style="A.BW.TLabel")
+passentry = Entry(cadretexte, textvariable="password", show="*", style="A.BW.TLabel")
+
+logo.grid(row=0, column=0, columnspan=2)
+titrelabel.grid(row=1, column=0, columnspan=2, pady=15)
+userlabel.grid(row=2, column=0, sticky="ew", padx=15)
+userentry.grid(row=2, column=1, sticky="ew", padx=15)
+passlabel.grid(row=3, column=0, sticky="ew", padx=15)
+passentry.grid(row=3, column=1, sticky="ew", padx=15, pady=5)
+
+userentry.focus()
 
 root.mainloop()
