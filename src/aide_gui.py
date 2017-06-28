dbf6721763c7012161df0a0098fb80545831e0df
SoFolichon <gabriel.dugny@gmail.com>
2017-06-28 18:44:47 +0200

    Ajout menu d'aide FIX

--- src/aide_gui.py
+++ src/aide_gui.py
@@ -1,19 +1,18 @@
 import tkinter as tk
 from tkinter.ttk import *
-import os
+
 import logger_conf
 import path_finder
-from ITwython import User
-from token_manager import delete_tokens
 
 logger = logger_conf.Log.logger
 
 
-class FenetreOptions(tk.Toplevel):
-    def __init__(self, parent, utilisateur, test=False):
+class FenetreAide(tk.Toplevel):
+    def __init__(self, parent):
         tk.Toplevel.__init__(self, parent)
         self.parent = parent
-
+        self.config(bg='#343232')
+        self.minsize(width=200, height=100)
         self.overrideredirect(False)
 
         self.title("Twysn")
@@ -21,11 +20,6 @@ class FenetreOptions(tk.Toplevel):
         frozen, chemin_absolu = path_finder.PathFinder.get_icon_path()
         icon = tk.PhotoImage(file=chemin_absolu)
         self.tk.call('wm', 'iconphoto', self._w, icon)
-
-        self.test = test
-
-        self.utilisateur = utilisateur
-        nom_at = "@" + self.utilisateur.screen_name
 
         style = Style()
         style.configure("TLabel", foreground="white", background="#343232", font=('Segoe UI', 10))
@@ -39,53 +33,19 @@ class FenetreOptions(tk.Toplevel):
         cadre = Frame(fenetre)
         cadre.grid(row=0, column=0, padx=40, pady=20)
 
-        titreoptions = Label(cadre, text="Options", font=('Segoe UI Semilight', 24))
+        titre = Label(cadre, text="Twysn", font=('Segoe UI Semilight', 24))
+        version = Label(cadre, text="Version : 1.1")
 
-        message_cache = Label(cadre, text="Supprimer les données en cache.")
-        separateur_cache = Label(cadre, text="")
-        bouton_cache = Button(cadre, text="Supprimer", command=FenetreOptions.supprimer_cache())
-        # bouton_cache.state(["disabled"])
-
-        message_deconnexion = Label(cadre, text="Connecté au compte {0}".format(nom_at))
-        bouton_deconnexion = Button(cadre, command=self.deconnexionquitter, text="Se déconnecter et quitter")
-
-        titreoptions.grid(column=0, row=0, sticky="w", pady=10)
-
-        message_cache.grid(row=1, column=0, columnspan=2, sticky="w")
-        separateur_cache.grid(row=1, column=2, padx=20, sticky="w")
-        bouton_cache.grid(row=1, column=3, pady=20, sticky="e")
-
-        message_deconnexion.grid(row=6, column=0, sticky="w", columnspan=2)
-        bouton_deconnexion.grid(row=6, column=3, sticky="e", columnspan=2)
-
-    def deconnexionquitter(self):
-        try:
-            delete_tokens() # de token_manager
-            self.parent.parent.parent.quit()  # (Principal dans main_app)
-            self.destroy()
-        except:
-            logger.error("Impossible de quitter.")
-            pass
-
-    @staticmethod
-    def supprimer_cache():
-        repertoire = path_finder.PathFinder.get_cache_directory()
-        for fichier in os.listdir(repertoire):
-            chemin_fichier = os.path.join(repertoire, fichier)
-            try:
-                if os.path.isfile(chemin_fichier):
-                    os.unlink(chemin_fichier)
-            except Exception as e:
-                logger.error('Impossible de supprimer le cache ! ' + e)
+        titre.grid(row=0, column=0)
+        version.grid(column=0, row=1)
+        # TODO Ajouter un texte avec, email, lien github, licenses, mails, description sommaire
 
 
 # Permet d'éxécuter le code uniquement si lancé
 # Pour tester
 if __name__ == "__main__":
-    from dev_assets import user_example
-
     root = tk.Tk()
-    r = FenetreOptions(root, User(user_example.c), test=True)
+    r = FenetreAide(root)
     r.grab_set()
     # root.call('tk', 'scaling', 2.0)
     root.mainloop()
